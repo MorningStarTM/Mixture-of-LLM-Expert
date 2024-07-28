9470fe4b1a73e552d00a10b39d9822d991234ed3
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-29 00:00:49 +0530

    sparse moe generate function created

--- MoE/moe.py
+++ MoE/moe.py
@@ -1,11 +1,14 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from .expert import Expert
 from .router import NoisyTopkRouter
+from .blocks import Head, MultiHeadAttention
+from .const import *
 
-
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 class SparseMoE(nn.Module):
-    def __init__(self, n_embed, num_experts, top_k):
+    def __init__(self):
         super(SparseMoE, self).__init__()
         self.router = NoisyTopkRouter(n_embed, num_experts, top_k)
         self.experts = nn.ModuleList([Expert(n_embed) for _ in range(num_experts)])
@@ -37,3 +40,74 @@ class SparseMoE(nn.Module):
                 final_output[expert_mask] += weighted_output.squeeze(1)
 
         return final_output
+    
+
+class Block(nn.Module):
+    """ Mixture of Experts Transformer block: communication followed by computation (multi-head self attention + SparseMoE) """
+
+    def __init__(self, n_embed, n_head, num_experts, top_k):
+        # n_embed: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embed // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.smoe = SparseMoE(n_embed, num_experts, top_k)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.smoe(self.ln2(x))
+        return x
+    
+
+
+class SparseMoELanguageModel(nn.Module):
+
+    def __init__(self, vocab_size, n_embed, block_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head=n_head, num_experts=num_experts,top_k=top_k) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embed) # final layer norm
+        self.lm_head = nn.Linear(n_embed, vocab_size)
+
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+    
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
