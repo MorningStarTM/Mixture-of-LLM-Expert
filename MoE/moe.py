bec5704981780786028bf16e447d053aac634b13
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-28 21:39:01 +0530

    moe created

---
+++ MoE/moe.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from .gating import Gating
+
+
+class MoE(nn.Module):
+    def __init__(self, trained_experts) -> None:
+        super(MoE, self).__init__()
+        self.experts = nn.ModuleList(trained_experts)
+
+        for expert in self.experts:
+            for param in expert.parameters():
+                param.requires_grad = False
+        
+        num_experts = len(trained_experts)
+
+        input_dim = trained_experts[0].layer1.in_features
+        self.gating = Gating(input_dim, num_experts)
+
+    def forward(self, x):
+        weights = self.gating(x)
+
+        outputs = torch.stack(
+            [expert(x) for expert in self.experts], dim=2
+        )
+
+        weights = weights.unsqueeze(1).expand_as(outputs)
+
+        return torch.sum(outputs * weights, dim=2)
