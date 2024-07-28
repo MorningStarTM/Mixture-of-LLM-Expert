3fedbb54a347ac74cd0046cb2fdd58f11f8a850a
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-28 23:23:37 +0530

    topk router created

---
+++ MoE/router.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TopkRouter(nn.Module):
+    def __init__(self, n_embed, num_experts, top_k):
+        super(TopkRouter, self).__init__()
+        self.top_k = top_k
+        self.linear = nn.Linear(n_embed, num_experts)
+
+    def forward(self, mh_output):
+        # mh_ouput is the output tensor from multihead self attention block
+        logits = self.linear(mh_output)
+        top_k_logits, indices = logits.topk(self.top_k, dim=-1)
+        zeros = torch.full_like(logits, float('-inf'))
+        sparse_logits = zeros.scatter(-1, indices, top_k_logits)
+        router_output = F.softmax(sparse_logits, dim=-1)
+        return router_output, indices
